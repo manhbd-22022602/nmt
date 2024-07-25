bf4ea5c84177d19904d1972d282fae44671841a7
manhbd-22022602 <22022602@vnu.edu.vn>
2024-07-25 14:49:37 +0700

    Change nmt.py to use tensorflow v2.xx version

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -23,7 +23,8 @@ import sys
 
 # import matplotlib.image as mpimg
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 from . import inference
 from . import train
