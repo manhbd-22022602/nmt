649d31b14b001924f2710dc41a7fcb7224e009e4
manhbd-22022602 <22022602@vnu.edu.vn>
2024-07-25 16:00:27 +0700

    Enable check_tensorflow_version

--- nmt/utils/misc_utils.py
+++ nmt/utils/misc_utils.py
@@ -30,13 +30,13 @@ import six
 import tensorflow as tf
 
 
-# def check_tensorflow_version():
-#   # LINT.IfChange
-#   min_tf_version = "1.12.0"
-#   # LINT.ThenChange(<pwd>/nmt/copy.bara.sky)
-#   if (version.LooseVersion(tf.__version__) <
-#       version.LooseVersion(min_tf_version)):
-#     raise EnvironmentError("Tensorflow version must >= %s" % min_tf_version)
+def check_tensorflow_version():
+  # LINT.IfChange
+  min_tf_version = "1.12.0"
+  # LINT.ThenChange(<pwd>/nmt/copy.bara.sky)
+  if (version.LooseVersion(tf.__version__) <
+      version.LooseVersion(min_tf_version)):
+    raise EnvironmentError("Tensorflow version must >= %s" % min_tf_version)
 
 
 def safe_exp(value):
